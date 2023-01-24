82261349b3e44614424a01d85a0ba8a6380f8fcc
tao <taoliudesign@gmail.com>
2023-01-23 18:35:17 -0800

    cleaup

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
-
 import { useState, useEffect } from 'react'
 
-
-
-
 function App() {
-
-
   const [ loggedInUser , setLoggedInUser ] = useState( null )
-  console.log( "STATE OF  loggedInUser: ", loggedInUser )  //
-  //// const [ currentUser , setCurrentUser ] = useState( null )
+  console.log( "STATE OF  loggedInUser: ", loggedInUser )  
   
   useEffect(  
     ()=>{
 
-      // console.log("👀 BYEBUG!!!!")  //
-
       fetch( "/userInSession" )
       .then( r => r.json() )
       .then( userAlreadyLoggedIn => { 
-        
         setLoggedInUser(userAlreadyLoggedIn) 
-    
-      
       })
       // .then( setLoggedInUser )
       // .then( console.log )
-
     }
     , []
   )
 
-
-  
-
-
-
   const [ userToLogin , updateUserToLoginInfo ] = useState(
-
     {
-
       username: "",
       password: ""
-
     }
-
   )
-  console.log( "STATE OF  userToLogin: ", userToLogin )  //
-
-
-
-
 
   const handleOnChangeForUserToLogin =( sythEvent )=>{
-
-    console.log( sythEvent ) //
-
     updateUserToLoginInfo(  { ...userToLogin , [sythEvent.target.name]: sythEvent.target.value }  )
-
-    //// sythEvent.target.name == the name property of the input field being typed in
-    //// sythEvent.target.value == what is being typed in that input field
-
   }
 
   const handleLoginSubmit =( sythEvent )=>{ 
     sythEvent.preventDefault()
-
-      console.log("👀 BYEBUG!!!!")  //
-
     fetch( "/login", 
       {
         method: "POST",
@@ -81,13 +43,8 @@ function App() {
     )
     .then( r => r.json() )
     .then( hopefullyAUser => { 
-      
       console.log(hopefullyAUser)  //
-
-
       setLoggedInUser( hopefullyAUser )
-    
-  
     })
   }
 
@@ -95,12 +52,9 @@ function App() {
     fetch(  "/logout" , { method: "DELETE" }  )
     .then( r => r.json() )
     .then( deleteResponse =>{
-
       //// Pesimistic Rendering 
         setLoggedInUser( null )
       //// Pesimistic Rendering 
-
-
     })
   }
 
@@ -108,45 +62,27 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {
-
-          loggedInUser ?
-                    
+        { loggedInUser ?
             <h2>Welcome { loggedInUser.name }! :)</h2> 
+            :
+            <>
+              <h1>Welcome! Login?</h1>
+              <form onSubmit={ handleLoginSubmit } >
 
-          :
-          (<>
-          
-            <h1>Welcome! Login?</h1>
-
-            <form onSubmit={ handleLoginSubmit } >
-
-                <label>username</label>
-                <input 
-                
-                  onChange={ handleOnChangeForUserToLogin } 
-                  name="username"
-                
-                />
-      
-                <label>password</label>
-                <input type="password"
-
-                  onChange={ handleOnChangeForUserToLogin } 
-                  name="password"
-
-                />
-
-
-              <input type="submit" />
-              {/* <button type="submit" /> */}
-
-
-            </form>
-          
-          </>)  
-          // False Case
-
+                  <label>username</label>
+                  <input 
+                    onChange={ handleOnChangeForUserToLogin } 
+                    name="username"
+                  />
+        
+                  <label>password</label>
+                  <input type="password"
+                    onChange={ handleOnChangeForUserToLogin } 
+                    name="password"
+                  />
+                <input type="submit" />
+              </form>
+            </>
         }
       </header>
     </div>
